fe165afb67969a3ed854adf8f887dd7acf49075c
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-07-16 22:57:44 +0330

    Add project files.

---
+++ YoutifyBot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YoutifyBot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ YoutifyBot/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace YoutifyBot.Models;
+
+public class User
+{
+    public required long ChatId { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public  string? Username { get; set; }
+}
