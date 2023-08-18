4b5ffa754329fa735d78e04da1bd49b2dc1625db
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-08-18 16:53:33 +0330

    Added UsersController

---
+++ YoutifyBot/Areas/Management/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YoutifyBot.Models.Repository;
+
+namespace YoutifyBot.Areas.Management.Controllers;
+
+public class UsersController : Controller
+{
+    IUnitOfWork unitOfWork;
+    public UsersController(IUnitOfWork unitOfWork) => this.unitOfWork = unitOfWork;
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
