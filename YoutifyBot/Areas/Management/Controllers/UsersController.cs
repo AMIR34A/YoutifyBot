dc6a59ff248727d707e94f5f8a770dfd71402782
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-08-19 20:05:49 +0330

    Added Index view of UsersController

--- YoutifyBot/Areas/Management/Controllers/UsersController.cs
+++ YoutifyBot/Areas/Management/Controllers/UsersController.cs
@@ -1,15 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ReflectionIT.Mvc.Paging;
+using YoutifyBot.Areas.Management.Models.ViewModels;
+using YoutifyBot.Models;
 using YoutifyBot.Models.Repository;
 
 namespace YoutifyBot.Areas.Management.Controllers;
 
+[Area("Management")]
 public class UsersController : Controller
 {
     IUnitOfWork unitOfWork;
     public UsersController(IUnitOfWork unitOfWork) => this.unitOfWork = unitOfWork;
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index(int pageSize = 10,int pageIndex=1)
     {
-        return View();
+        var users = await unitOfWork.Repository<User>().GetAllAsync();
+
+        var paging = PagingList.Create(users.Select(
+            user => new UsersViewModel
+            {
+                ChatId = user.ChatId,
+                FirstName = string.IsNullOrEmpty(user.FirstName) ? "-" : user.FirstName,
+                LastName = string.IsNullOrEmpty(user.LastName) ? "-" : user.LastName,
+                Username = string.IsNullOrEmpty(user.Username) ? "-" : user.Username,
+                MaximumDownloadSize = user.MaximumDownloadSize,
+                TotalDonwload = user.TotalDonwload,
+                UserRole = user.UserRole
+            }), pageSize, pageIndex);
+
+        paging.RouteValue = new RouteValueDictionary
+        {
+            { "pagesize" , pageSize }
+        };
+        return View(paging);
     }
 }
---
+++ YoutifyBot/Areas/Management/Models/ViewModels/UsersViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using YoutifyBot.Models;
+
+namespace YoutifyBot.Areas.Management.Models.ViewModels;
+
+public class UsersViewModel
+{
+    public required long ChatId { get; set; }
+    [Display(Name ="First name")]
+    public string? FirstName { get; set; }
+    [Display(Name = "Last name")]
+    public string? LastName { get; set; }
+    [Display(Name = "Username")]
+    public string? Username { get; set; }
+    [Display(Name = "Maximum Download")]
+    public int MaximumDownloadSize { get; set; }
+    [Display(Name = "Total Donwload")]
+    public int TotalDonwload { get; set; }
+    [Display(Name = "Role")]
+    public Role UserRole { get; set; }
+}
