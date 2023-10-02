5a609f597a9a8e629f55a0285dcf26e7289a16ae
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-10-02 15:11:54 +0330

    Add Index page to RuleController

---
+++ YoutifyBot/Areas/Management/Controllers/RuleController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YoutifyBot.Areas.Management.Models.ViewModels;
+using YoutifyBot.Models;
+using YoutifyBot.Models.Repository;
+
+namespace YoutifyBot.Areas.Management.Controllers;
+
+[Area("Management")]
+public class RuleController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RuleController(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+    public async Task<IActionResult> Index()
+    {
+        var rule = await _unitOfWork.Repository<Rule>().GetFirstAsync();
+        var ruleViewModel = new RuleViewModel
+        {
+            RuleId = rule.RuleId,
+            BaseDownloadSize = rule.BaseDownloadSize,
+            MaximumDownloadSize = rule.MaximumDownloadSize,
+            AmountRewardInviting = rule.AmountRewardInviting,
+            IsNecessaryJoinActive = rule.IsNecessaryJoinActive,
+            NecessaryJoinChannels = rule.NecessaryJoinChannels,
+            NecessaryJoinFor = rule.NecessaryJoinFor
+        };
+        return View(ruleViewModel);
+    }
+}
