b89568edaf5292ffa0db5b5e3c882d1de60171dd
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-11-25 21:22:07 +0330

    Delete Index action method and add LogIn

--- YoutifyBot/Areas/Management/Controllers/AccountController.cs
+++ YoutifyBot/Areas/Management/Controllers/AccountController.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace YoutifyBot.Areas.Management.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult LogIn() => View();
     }
 }
