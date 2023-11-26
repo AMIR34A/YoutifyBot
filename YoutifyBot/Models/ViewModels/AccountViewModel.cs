4b337db8945ae07a0b185f610aed0156b2d71076
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-11-26 18:10:12 +0330

    Add AccountViewModel

---
+++ YoutifyBot/Models/ViewModels/AccountViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YoutifyBot.Models.ViewModels
+{
+    public class LogInViewModel
+    {
+        [Required(ErrorMessage ="Please Enter the Username!")]
+        [Display(Name ="Username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please Enter the Password!")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
