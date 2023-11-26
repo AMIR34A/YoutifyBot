83995ed915083aaabc905508700ee30b1251c95e
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-11-26 18:10:42 +0330

    Add Area attribute for AccountController

--- YoutifyBot/Areas/Management/Controllers/AccountController.cs
+++ YoutifyBot/Areas/Management/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace YoutifyBot.Areas.Management.Controllers
 {
+    [Area("Management")]
     public class AccountController : Controller
     {
         public IActionResult LogIn() => View();
