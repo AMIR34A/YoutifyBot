92c58c2529a13b9bb6a54cdb282ed9744661a08c
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-11-27 19:00:09 +0330

    Add UserManager and SignInManager for dependency injection

--- YoutifyBot/Areas/Management/Controllers/AccountController.cs
+++ YoutifyBot/Areas/Management/Controllers/AccountController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using YoutifyBot.Models.ViewModels;
 
 namespace YoutifyBot.Areas.Management.Controllers
 {
     [Area("Management")]
     public class AccountController : Controller
     {
+        UserManager<IdentityUser> _userManager;
+        SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult LogIn() => View();
     }
